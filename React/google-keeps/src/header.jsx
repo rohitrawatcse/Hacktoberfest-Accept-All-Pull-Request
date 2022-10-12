e92d1961f25ff9dc60edefca09be915287e7047a
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2022-10-12 12:07:07 +0530

    Update header.jsx

--- React/google-keeps/src/header.jsx
+++ React/google-keeps/src/header.jsx
@@ -3,7 +3,7 @@ import PostAddIcon from '@material-ui/icons/PostAdd';
 const Header=()=>{
     return (
         <nav>
-            <h2>R Keeps</h2>
+            <h2>Keep Notes/h2>
             <PostAddIcon style={{fontSize:'5rem' }}/>
             <a href='https://ishubhamrana.github.io/index.html' target='_blank'>
             <img src='src\Capture.PNG' alt='Developer' />
